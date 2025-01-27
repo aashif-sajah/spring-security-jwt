8f8a764fcf0addb10896ffad80690103a441f257
Aashif Sajah <masaashifsajah@gmail.com>
2025-01-27 08:45:31 +0530

    Completed home ui

--- frontend/spring-security-jwt/src/app/login/login.component.ts
+++ frontend/spring-security-jwt/src/app/login/login.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  userName: string = '';
+  userPassword: string = '';
 
+  onSubmit(){
+    console.log(this.userName);
+    console.log(this.userPassword);
+  }
 }
