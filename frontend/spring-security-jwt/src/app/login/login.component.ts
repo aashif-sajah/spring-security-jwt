3c707a4feca7f8947e31f62638413c9c25dccda9
Aashif Sajah <masaashifsajah@gmail.com>
2025-01-28 09:20:12 +0530

    Added bootstrap

--- frontend/spring-security-jwt/src/app/login/login.component.ts
+++ frontend/spring-security-jwt/src/app/login/login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent
+{
+  userName: string = '';
+  userPassword: string = '';
 
+  onSubmit(){
+    console.log('Username: ' + this.userName + ' Password: ' + this.userPassword);
+  }
 }
