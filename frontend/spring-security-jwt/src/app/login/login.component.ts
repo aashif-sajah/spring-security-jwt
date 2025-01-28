b67185fe06bfd9c1ff6ea955af85ffe4d77416be
Aashif Sajah <masaashifsajah@gmail.com>
2025-01-28 11:13:05 +0530

    Added auth service

--- frontend/spring-security-jwt/src/app/login/login.component.ts
+++ frontend/spring-security-jwt/src/app/login/login.component.ts
@@ -1,19 +1,37 @@
+import { JwtRequest } from './../models/jwt-request.model';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { AuthService } from '../authService/auth.service';
+import { Router } from '@angular/router';
+import { JwtResponse } from '../models/jwt-response.model';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent
-{
-  userName: string = '';
-  userPassword: string = '';
+export class LoginComponent {
+  jwtRequest: JwtRequest = new JwtRequest();
 
-  onSubmit(){
-    console.log('Username: ' + this.userName + ' Password: ' + this.userPassword);
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      this.authService.authenticate(this.jwtRequest).subscribe(
+        (jwtResponse: JwtResponse) => {
+          console.log(jwtResponse);
+
+          localStorage.setItem('jwtToken', jwtResponse.jwtToken);
+          localStorage.setItem('user', JSON.stringify(jwtResponse.user));
+          this.router.navigate(['/home']);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
   }
 }
